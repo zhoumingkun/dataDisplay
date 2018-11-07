d3ac73fce8c0be1bd1883158578c6e8dcff6f4f6
BOBO <1192012793@qq.com>
2018-11-07 09:39:27 +0800

    获取微信openId

--- src/main/java/com/toughguy/reportingSystem/controller/business/WeixinController.java
+++ src/main/java/com/toughguy/reportingSystem/controller/business/WeixinController.java
@@ -1,59 +1,45 @@
 package com.toughguy.reportingSystem.controller.business;
 
-import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import me.chanjar.weixin.common.exception.WxErrorException;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.result.WxMpOAuth2AccessToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.toughguy.reportingSystem.util.HttpUtil;
+import lombok.extern.slf4j.Slf4j;
 
+ 
 @RestController
-@RequestMapping("/apiwx")
+@RequestMapping("/wechat")
+@Slf4j
 public class WeixinController {
-    
-
-	private static Logger log = LoggerFactory.getLogger(WeixinController.class);
-
-	@Value("${wx.appId}")
-	private String appId;
-
-	@Value("${wx.appSecret}")
-	private String appSecret;
-
-	@Value("${wx.grantType}")
-	private String grantType;
-	// wx.grantType=authorization_code
-
-	@Value("${wx.requestUrl}")
-	private String requestUrl;
-	// wx.requestUrl=https://api.weixin.qq.com/sns/jscode2session
-
-	@RequestMapping("/session")
-	public Map<String, Object> getSession(@RequestParam(required = true) String code) {
-		return this.getSessionByCode(code);
-	}
-
-	@SuppressWarnings("unchecked")
-	private Map<String, Object> getSessionByCode(String code) {
-		String url = this.requestUrl + "?appid=" + appId + "&secret=" + appSecret + "&js_code=" + code + "&grant_type="
-				+ grantType;
-		// 发送请求
-		String data = HttpUtil.get(url);
-		log.debug("请求地址：{}", url);
-		log.debug("请求结果：{}", data);
-		ObjectMapper mapper = new ObjectMapper();
-		Map<String, Object> json = null;
-		try {
-			json = mapper.readValue(data, Map.class);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		// 形如{"session_key":"6w7Br3JsRQzBiGZwvlZAiA==","openid":"oQO565cXXXXXEvc4Q_YChUE8PqB60Y"}的字符串
-		return json;
-	}
+ 
+    @Autowired
+    private WxMpService wxMpService;
+ 
+//    @GetMapping("/authorize")
+//    public String authorize(@RequestParam("returnUrl") String returnUrl){
+//        String url = "http://localhost:8082/reportingSystem/wechat/userInfo";
+//        String redirectURL = wxMpService.oauth2buildAuthorizationUrl(url, WxConsts.OAuth2Scope.SNSAPI_USERINFO, URLEncoder.encode(returnUrl));
+//        return "redirect:" + redirectURL;
+//    }
+ 
+    @GetMapping("/userInfo")
+    public String userInfo(@RequestParam("code") String code) throws Exception {
+        WxMpOAuth2AccessToken wxMpOAuth2AccessToken;
+        try {
+            wxMpOAuth2AccessToken = wxMpService.oauth2getAccessToken(code);
+        } catch (WxErrorException e) {
+        	throw new Exception(e.getError().getErrorMsg());
+        }
+        String openId = wxMpOAuth2AccessToken.getOpenId();
+        System.out.println(openId);
+        return openId;
+    }
 }
+
+
