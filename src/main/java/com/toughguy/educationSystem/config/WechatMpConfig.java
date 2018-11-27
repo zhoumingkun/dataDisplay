d860d53de86fc676326b985342481cce751c471b
zhoumingkun <573206019@qq.com>
2018-11-27 14:18:43 +0800

    //

---
+++ src/main/java/com/toughguy/educationSystem/config/WechatMpConfig.java
@@ -0,0 +1,31 @@
+package com.toughguy.educationSystem.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import me.chanjar.weixin.mp.api.WxMpConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpInMemoryConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
+
+@Component
+public class WechatMpConfig {
+	 @Autowired
+	    private WechatAccountConfig wechatAccountConfig;
+	 
+	    @Bean
+	    public WxMpService wxMpService(){
+	        WxMpService wxMpService = new WxMpServiceImpl();
+	        wxMpService.setWxMpConfigStorage(wxMpConfigStorage());
+	        return wxMpService;
+	    }
+	 
+	    @Bean
+	    public WxMpConfigStorage wxMpConfigStorage(){
+	        WxMpInMemoryConfigStorage wxMpConfigStorage = new WxMpInMemoryConfigStorage();
+	        wxMpConfigStorage.setAppId(wechatAccountConfig.getMpAppId());
+	        wxMpConfigStorage.setSecret(wechatAccountConfig.getMpAppSecret());
+	        return wxMpConfigStorage;
+	    }
+}
