80990c18afc887cf1a073f199f26dcf43adff19a
sanhaoqingnian <su2426694419@qq.com>
2019-09-03 16:59:25 +0800

    //新增帮助前端获取json文件的方法

--- src/main/java/com/toughguy/dataDisplay/controller/content/DictBJFSDMBController.java
+++ src/main/java/com/toughguy/dataDisplay/controller/content/DictBJFSDMBController.java
@@ -1,5 +1,10 @@
 package com.toughguy.dataDisplay.controller.content;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -9,10 +14,14 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.annotations.Param;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +34,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.toughguy.dataDisplay.model.content.DictBJFSDMB;
 import com.toughguy.dataDisplay.pagination.PagerModel;
 import com.toughguy.dataDisplay.service.content.prototype.IDictBJFSDMBService;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+
+
 
 @RestController
 @RequestMapping(value = "/dictBJFSDMB")
@@ -40,8 +55,31 @@ public class DictBJFSDMBController {
 	@ResponseBody
 	@RequestMapping(value = "/getAll")
 //	@RequiresPermissions("dictXZQHB:getById")
-	public List<DictBJFSDMB> getAll() {
-		return  dictBJFSDMBService.findAll();
+	public String getAll(String json) {
+		 String Path="C:\\json\\" + json+ ".json";
+		 BufferedReader reader = null;
+	     String laststr = "";
+	     try {
+	          FileInputStream fileInputStream = new FileInputStream(Path);
+	          InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
+	          reader = new BufferedReader(inputStreamReader);
+	          String tempString = null;
+	          while ((tempString = reader.readLine()) != null) {
+	              laststr += tempString;
+	          }
+	          reader.close();
+	     } catch (IOException e) {
+	         e.printStackTrace();
+	        } finally {
+	           if (reader != null) {
+	               try {
+	                 reader.close();
+	             } catch (IOException e) {
+	                 e.printStackTrace();
+	                }
+	           }
+	       }
+	       return laststr;
 	}
 	
 
