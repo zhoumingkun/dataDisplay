d0ea1521faa5be6034182cdddf03580e4eedd821
sanhaoqingnian <su2426694419@qq.com>
2019-11-29 15:39:30 +0800

    //新增接警单表实体类

---
+++ src/main/java/com/toughguy/dataDisplay/model/content/JJDB.java
@@ -0,0 +1,227 @@
+package com.toughguy.dataDisplay.model.content;
+
+import java.util.Date;
+
+public class JJDB {
+	private String XZQHDM;		//行政区划代码
+	private String JJDWDM;		//接警单位代码
+	private String JJDBH;		//接警单编号
+	private String JJLX;		//接警类型
+	private String BJFSDM;		//报警方式
+	private String LHLX;		//来话类型代码
+	private Date BJSJ;			//报警时间
+	private Date JJSJ;			//接警时间
+	private Date WCSJ;			//接警完成时间
+	private String BJDH;		//报警电话
+	private String BJDHYHXM;	//报警电话用户姓名
+	private String BJDHYHDZ;	//报警电话用户地址
+	private String BJRXM;		//报警人姓名
+	private String JQDZ;		//警情地址
+	private String BJNR;		//报警内容
+	private String JQLBDM;		//警情分类代码
+	private String JQLXDM;		//警情类型代码
+	private String JQXLDM;		//警情细类代码
+	private int YWWXWZ;			//有无危险物质
+	private int YWBZXL;			//有无爆炸/泄漏
+	private String BKRYQKSM;	//被困人员情况说明
+	private String SSRYQKSM;	//受伤人员情况说明
+	private String SWRYQKSM;	//死亡人员情况说明
+	private int BJRXZB;			//报警人X坐标
+	private int BJRYZB;			//报警人Y坐标
+	private String JQDJDM;		//警情等级代码
+	private String SJCPH;		//涉及车牌号
+	private int SFSWHCL;		//是否是危化车辆
+	private int GXBS;			//更新标识
+	private Date GXSJ;			//更新时间
+	public String getXZQHDM() {
+		return XZQHDM;
+	}
+	public void setXZQHDM(String xZQHDM) {
+		XZQHDM = xZQHDM;
+	}
+	public String getJJDWDM() {
+		return JJDWDM;
+	}
+	public void setJJDWDM(String jJDWDM) {
+		JJDWDM = jJDWDM;
+	}
+	public String getJJDBH() {
+		return JJDBH;
+	}
+	public void setJJDBH(String jJDBH) {
+		JJDBH = jJDBH;
+	}
+	public String getJJLX() {
+		return JJLX;
+	}
+	public void setJJLX(String jJLX) {
+		JJLX = jJLX;
+	}
+	public String getBJFSDM() {
+		return BJFSDM;
+	}
+	public void setBJFSDM(String bJFSDM) {
+		BJFSDM = bJFSDM;
+	}
+	public String getLHLX() {
+		return LHLX;
+	}
+	public void setLHLX(String lHLX) {
+		LHLX = lHLX;
+	}
+	public Date getBJSJ() {
+		return BJSJ;
+	}
+	public void setBJSJ(Date bJSJ) {
+		BJSJ = bJSJ;
+	}
+	public Date getJJSJ() {
+		return JJSJ;
+	}
+	public void setJJSJ(Date jJSJ) {
+		JJSJ = jJSJ;
+	}
+	public Date getWCSJ() {
+		return WCSJ;
+	}
+	public void setWCSJ(Date wCSJ) {
+		WCSJ = wCSJ;
+	}
+	public String getBJDH() {
+		return BJDH;
+	}
+	public void setBJDH(String bJDH) {
+		BJDH = bJDH;
+	}
+	public String getBJDHYHXM() {
+		return BJDHYHXM;
+	}
+	public void setBJDHYHXM(String bJDHYHXM) {
+		BJDHYHXM = bJDHYHXM;
+	}
+	public String getBJDHYHDZ() {
+		return BJDHYHDZ;
+	}
+	public void setBJDHYHDZ(String bJDHYHDZ) {
+		BJDHYHDZ = bJDHYHDZ;
+	}
+	public String getBJRXM() {
+		return BJRXM;
+	}
+	public void setBJRXM(String bJRXM) {
+		BJRXM = bJRXM;
+	}
+	public String getJQDZ() {
+		return JQDZ;
+	}
+	public void setJQDZ(String jQDZ) {
+		JQDZ = jQDZ;
+	}
+	public String getBJNR() {
+		return BJNR;
+	}
+	public void setBJNR(String bJNR) {
+		BJNR = bJNR;
+	}
+	public String getJQLBDM() {
+		return JQLBDM;
+	}
+	public void setJQLBDM(String jQLBDM) {
+		JQLBDM = jQLBDM;
+	}
+	public String getJQLXDM() {
+		return JQLXDM;
+	}
+	public void setJQLXDM(String jQLXDM) {
+		JQLXDM = jQLXDM;
+	}
+	public String getJQXLDM() {
+		return JQXLDM;
+	}
+	public void setJQXLDM(String jQXLDM) {
+		JQXLDM = jQXLDM;
+	}
+	public int getYWWXWZ() {
+		return YWWXWZ;
+	}
+	public void setYWWXWZ(int yWWXWZ) {
+		YWWXWZ = yWWXWZ;
+	}
+	public int getYWBZXL() {
+		return YWBZXL;
+	}
+	public void setYWBZXL(int yWBZXL) {
+		YWBZXL = yWBZXL;
+	}
+	public String getBKRYQKSM() {
+		return BKRYQKSM;
+	}
+	public void setBKRYQKSM(String bKRYQKSM) {
+		BKRYQKSM = bKRYQKSM;
+	}
+	public String getSSRYQKSM() {
+		return SSRYQKSM;
+	}
+	public void setSSRYQKSM(String sSRYQKSM) {
+		SSRYQKSM = sSRYQKSM;
+	}
+	public String getSWRYQKSM() {
+		return SWRYQKSM;
+	}
+	public void setSWRYQKSM(String sWRYQKSM) {
+		SWRYQKSM = sWRYQKSM;
+	}
+	public int getBJRXZB() {
+		return BJRXZB;
+	}
+	public void setBJRXZB(int bJRXZB) {
+		BJRXZB = bJRXZB;
+	}
+	public int getBJRYZB() {
+		return BJRYZB;
+	}
+	public void setBJRYZB(int bJRYZB) {
+		BJRYZB = bJRYZB;
+	}
+	public String getJQDJDM() {
+		return JQDJDM;
+	}
+	public void setJQDJDM(String jQDJDM) {
+		JQDJDM = jQDJDM;
+	}
+	public String getSJCPH() {
+		return SJCPH;
+	}
+	public void setSJCPH(String sJCPH) {
+		SJCPH = sJCPH;
+	}
+	public int getSFSWHCL() {
+		return SFSWHCL;
+	}
+	public void setSFSWHCL(int sFSWHCL) {
+		SFSWHCL = sFSWHCL;
+	}
+	public int getGXBS() {
+		return GXBS;
+	}
+	public void setGXBS(int gXBS) {
+		GXBS = gXBS;
+	}
+	public Date getGXSJ() {
+		return GXSJ;
+	}
+	public void setGXSJ(Date gXSJ) {
+		GXSJ = gXSJ;
+	}
+	@Override
+	public String toString() {
+		return "JJDB [XZQHDM=" + XZQHDM + ", JJDWDM=" + JJDWDM + ", JJDBH=" + JJDBH + ", JJLX=" + JJLX + ", BJFSDM="
+				+ BJFSDM + ", LHLX=" + LHLX + ", BJSJ=" + BJSJ + ", JJSJ=" + JJSJ + ", WCSJ=" + WCSJ + ", BJDH=" + BJDH
+				+ ", BJDHYHXM=" + BJDHYHXM + ", BJDHYHDZ=" + BJDHYHDZ + ", BJRXM=" + BJRXM + ", JQDZ=" + JQDZ
+				+ ", BJNR=" + BJNR + ", JQLBDM=" + JQLBDM + ", JQLXDM=" + JQLXDM + ", JQXLDM=" + JQXLDM + ", YWWXWZ="
+				+ YWWXWZ + ", YWBZXL=" + YWBZXL + ", BKRYQKSM=" + BKRYQKSM + ", SSRYQKSM=" + SSRYQKSM + ", SWRYQKSM="
+				+ SWRYQKSM + ", BJRXZB=" + BJRXZB + ", BJRYZB=" + BJRYZB + ", JQDJDM=" + JQDJDM + ", SJCPH=" + SJCPH
+				+ ", SFSWHCL=" + SFSWHCL + ", GXBS=" + GXBS + ", GXSJ=" + GXSJ + "]";
+	}
+
+}
